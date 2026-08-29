4f12c973b8062303f6d439dd9ce2a4b336ac166b
Darkhan-Weather <examples.code@gmail.com>
2026-08-29 09:56:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    Эс бөгөөс хэрэглэгч хуучин хувилбарт гацна.
    ═══════════════════════════════════════════════════════════════ */
 
-const VERSION = 'v1.6.6';
+const VERSION = 'v1.6.7';
 const SHELL_CACHE  = 'hb-shell-'  + VERSION;
 const ASSET_CACHE  = 'hb-assets-' + VERSION;
 
